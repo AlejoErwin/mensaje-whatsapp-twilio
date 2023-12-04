7f3fc4bfa48cf8cc643e7ed4e124a51b174d3093
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2023-12-04 00:38:47 -0400

    se creo la conexcion con twilio

--- src/main/java/com/example/mensajewhatsapptwilio/services/SentMesaje.java
+++ src/main/java/com/example/mensajewhatsapptwilio/services/SentMesaje.java
@@ -9,21 +9,15 @@ import java.net.URI;
 import java.math.BigDecimal;
 
 public class SentMesaje {
-    // Find your Account Sid and Token at twilio.com/console
-//    public static final String ACCOUNT_SID = "AC4789b1b961be1700024eb618b6482251";
-//    public static final String AUTH_TOKEN = "8d2a68b5914d652b0adfba6e80947f1e";
 
-//    public static void main(String[] args) {
-//
-//    }
 
     public SentMesaje() {
     }
 
     public static void MendarMensaje(String numeroEmpleado,String mensaje){
 
-        String ACCOUNT_SID = "AC4789b1b961be1700024eb618b6482251";
-        String AUTH_TOKEN = "8d2a68b5914d652b0adfba6e80947f1e";
+        String ACCOUNT_SID = "CODIGO";
+        String AUTH_TOKEN = "CODIGO";
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                 new com.twilio.type.PhoneNumber("whatsapp:+"+numeroEmpleado),
@@ -36,7 +30,7 @@ public class SentMesaje {
 
 
     public static void main(String[] args) {
-        MendarMensaje("59178777365","Prueba Jorge");
+        MendarMensaje("5917777777","Prueba Dato");
     }
 
 
